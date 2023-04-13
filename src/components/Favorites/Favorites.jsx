1fa04df9548e17794c2468156afb9324ff2e0211
HolgadoJairoDavid <jairodavidholgado@gamil.com>
2023-04-13 07:40:13 -0300

    sixth commit 13/04

--- src/components/Favorites/Favorites.jsx
+++ src/components/Favorites/Favorites.jsx
@@ -1,41 +1,80 @@
-import { connect } from "react-redux";
-import Card from "../Card/Card";
-import { removeFav } from "../../redux/actions";
-import style from './favoritres.module.css'
 
-const Favorites = ({myFavorites, removeFav}) => {
+import Card from "../Card/Card";
+import { removeFav, filterCards, orderCards, all } from "../../redux/actions";
+import style from "./favoritres.module.css";
+import { useDispatch, connect } from "react-redux";
+import { useState } from "react";
+
+
+const Favorites = ({ myFavorites, removeFav }) => {
+  const dispatch = useDispatch();
+  const [aux, setAux] = useState(false);
+
+  const handleOrder = (event) => {
+      dispatch(orderCards(event.target.value));
+      setAux(!aux)
+  };
+
+  const handleFilter = (event) => {
     
+      dispatch(filterCards(event.target.value));
+    
+  
+  };
+
   return (
+   <div>
+     <div className={style.Options}>
+
+   
+   <select onChange={handleOrder}>
+     <option value="A">Ascendente</option>
+     <option value="D">Descendente</option>
+   </select>
+
+   <select onChange={handleFilter}>
+     
+     <option value="Male">Male</option>
+     <option value="Female">Female</option>
+     <option value="Genderless">Genderless</option>
+     <option value="unknown">unknown</option>
+   </select>
+   <button onClick={()=> {dispatch(all())}}>See all</button>
+</div>
     <div className={style.Container}>
-      {myFavorites.map((character) => {
+      {myFavorites.map((props) => {
         return (
           <Card
-            key= {character.id}
-            id={character.id}
-            name={character.name}
-            status={character.status}
-            image={character.image}
-            onClose={()=>{
-                removeFav(character.id)
-            }}
-          />
+                      id={props.id}
+                      name={props.name}
+                      status={props.status}
+                      species={props.species}
+                      gender={props.gender}
+                      origin={props.origin.name}
+                      image={props.image}
+                      onClose={() => {
+                          removeFav(props.id);
+                      }}
+                  />
         );
       })}
     </div>
+   </div>
   );
 };
 
 const mapStateToProps = (state) => {
   return {
-    myFavorites: state.myFavorites,
+      myFavorites: state.myFavorites,
   };
 };
 
-const mapDispatchToProps = (dispatch)=>{
-    return{
-        removeFav: (id)=>{dispatch(removeFav(id))}
-    }
-}
+const mapDispatchToProps = (dispatch) => {
+  return {
+      removeFav: (id) => {
+          dispatch(removeFav(id));
+      },
+  };
+};
 
 export default connect(mapStateToProps, mapDispatchToProps)(Favorites);
-
